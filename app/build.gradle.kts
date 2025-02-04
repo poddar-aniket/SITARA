2432ee27a618a374f1ef56db240d78c403a9c5c3
poddar-aniket <aniket.20243029@mnnit.ac.in>
2025-02-04 18:34:31 +0530

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
+
+}
+
+android {
+    namespace = "com.example.sitara"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.sitara"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildFeatures {
+        viewBinding=true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.auth.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+//    implementation ("com.google.firebase:firebase-bom:33.8.0")
+//    implementation("com.cloudinary:cloudinary-android:2.0.1")
+
+}
---
+++ app/src/main/java/com/example/sitara/signup.kt
@@ -0,0 +1,70 @@
+package com.example.sitara
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.sitara.databinding.ActivitySignupBinding
+import com.example.sitara.model.uploader
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
+
+class signup : AppCompatActivity() {
+    lateinit var binding: ActivitySignupBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding=ActivitySignupBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        binding.textView9.setOnClickListener {
+            var i=Intent(this,loginActivity::class.java)
+            startActivity(i)
+        }
+      binding.button.setOnClickListener {
+          signup()
+      }
+    }
+
+   fun signup(){
+        var email=binding.emailinpt.text.toString()
+        var pass=binding.passwordinpt.text.toString()
+        var use=binding.user.text.toString()
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.emailinpt.setError("email not valid")
+            return
+        }
+        if(pass.length<6){
+            binding.passwordinpt.setError("Minimum 6 characters")
+            return
+        }
+        signupwithfirebase(email,pass,use)
+
+    }
+    fun signupwithfirebase(email : String , pass :String,usname:String){
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+            email,pass
+        ).addOnSuccessListener {
+            Toast.makeText(applicationContext,"Success",Toast.LENGTH_SHORT).show()
+         it.user.let{ user ->
+             if(user!=null) {
+                 val usermod = uploader(user.uid, email, usname)
+                 Firebase.firestore.collection("users")
+                     .document(user.uid)
+                     .set(usermod).addOnSuccessListener {
+                         Toast.makeText(
+                             applicationContext,
+                             "Account created successfully",
+                             Toast.LENGTH_LONG
+                         ).show()
+                     }
+             }
+         }
+        }
+    }
+}
